f028e4d81f4a2660dd3cb8aab1ce2f912e2ac235
Ejaypro <ejaypro@mail.ru>
2021-09-09 12:03:52 +0300

    Homework-2
    II guess I need to check props of colorBg and urlbg differently. Next time I'll do it other way

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Layout from './components/Layout';
+
+import BgImage1 from './assets/bg1.jpg';
+import BgImage2 from './assets/bg3.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-      </header>
-    </div>
+    <>
+      <Header
+        title="Header title"
+        descr="Header description. Pokemon Game"
+      />
+      <Layout
+        title="First Layout"
+        descr="First layout description"
+        urlBg={BgImage1}   
+      />
+      <Layout
+        title="Second Layout"
+        descr="Second layout description"
+        colorBg="#F2F2F2"
+      />
+      <Layout
+        title="Third Layout"
+        descr="Third layout description"
+        urlBg={BgImage2}
+      />
+      <Footer />
+    </>
   );
 }
 
